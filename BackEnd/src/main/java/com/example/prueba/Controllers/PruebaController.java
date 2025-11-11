1a44c2d6c05127793f63460f74b7e596b930a0c2
sebastian-quiceno <sebastian.quicenol@upb.edu.co>
2025-11-11 15:17:40 -0500

    Creating a new branch for testing with Render

---
+++ BackEnd/src/main/java/com/example/prueba/Controllers/PruebaController.java
@@ -0,0 +1,15 @@
+package com.example.prueba.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class PruebaController {
+    @GetMapping("/probar")
+    public String probar() {
+        return "Esto es una prueba de endPoint";
+    }
+    
+}
